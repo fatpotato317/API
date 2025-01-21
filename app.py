b1d02b5483e48bd0cf7199dd6a8b6e38ff71d4fd
fatpotato317 <fatpotato317@gmail.com>
2025-01-21 16:46:01 +0530

    Just an Update, with a cool new homepage!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from supabase import create_client, Client
 import os
 import random
@@ -10,38 +10,36 @@ app = Flask(__name__)
 SUPABASE_URL = config("URL")
 SUPABASE_KEY = config("KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-#this is my get requests!
+
+# Home route to render the documentation
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Get requests
 @app.route('/<animal>', methods=['GET'])
 def get_random_picture(animal):
     if animal not in ['dog', 'cat', 'capybara']:
         return jsonify({"error": "Hey there! looks like you give me an invalid animal type. Please use 'dog', 'cat', or 'capybara'! (More info at https://github.com/fatpotato317/API)"}), 400
 
-
-  
     try:
         response = supabase.table(animal).select("*").execute()
         data = response.data
         if not data:
             return jsonify({"error": f"No pictures available for {animal}."}), 404
 
-
         random_picture = data[random.randint(0, len(data) - 1)]
         return jsonify({"url": random_picture['url']})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-
-
-
-#this is my post requests!
+# Post requests
 @app.route('/add_picture', methods=['POST'])
 def add_picture():
     data = request.json
     animal = data.get('animal')
     url = data.get('url')
 
-  
     if animal not in ['dog', 'cat', 'capybara']:
         return jsonify({"error": "Hey there! looks like you gave me an invalid animal type! Use 'dog', 'cat', or 'capybara'!(More info at https://github.com/fatpotato317/API)"}), 400
     if not url:
